c947cd9fcd3e08e62428c132d0dbdf623c511902
c64 <61917941+c-128@users.noreply.github.com>
2021-05-20 10:13:20 +0200

    Added GuiLib.init()

--- src/main/java/com/guilib/buttons/CloseButton.java
+++ src/main/java/com/guilib/buttons/CloseButton.java
@@ -8,14 +8,26 @@ import net.minestom.server.item.Material;
 
 public class CloseButton extends Button {
 
+    private ItemStack item;
+
     /**
      * A Button that closes the Inventory when you click on it
      */
     public CloseButton() {
     }
 
+    /**
+     * A Button that closes the Inventory when you click on it
+     *
+     * @param item the item for the button that will show up in the inventory
+     */
+    public CloseButton(ItemStack item) {
+        this.item = item;
+    }
+
     @Override
     public ItemStack getItem() {
+        if (item != null) return item;
         return ItemStack.builder(Material.BARRIER)
                 .displayName(Component.text("Exit", NamedTextColor.RED))
                 .lore(Component.text("Exit the GUI", NamedTextColor.GRAY))
